7c4fccbef82b394ab8bc11715445ce55b4329aa1
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-09 11:21:13 +0100

    login api

--- util/auth.js
+++ util/auth.js
@@ -2,15 +2,22 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyDKzz6jaxjckQm9CnnUqlKu17VscfvM0ew";
 
-export async function createUser(email, password) {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
-  //   return response;
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+}
+
+export async function createUser(email, password) {
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
